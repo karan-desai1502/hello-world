07b42e41b3985dfa5457e9e66f0ed411898800d8
karan-desai1502 <86946617+karan-desai1502@users.noreply.github.com>
2021-07-17 15:04:32 +0530

    Hello World in Java!!

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package hello.world;
+/**
+ * 
+ * karan-desai1502
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World in Java!!");
+    }
+    
+}
